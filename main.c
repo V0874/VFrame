e17fa1dc3a3fde7d51d9069f397d0fc4f5954f24
V0874 <visthemasteranon@gmail.com>
2025-09-09 02:02:28 -0500

    we were able to get two way communication going and echo back our characters, as well as get a mini command interpreter going. we can only turn an led on and off for now but we will expand the functionality to support full string commands.

--- main.c
+++ main.c
@@ -2,53 +2,74 @@
 #include "gpio/gpio.h"
 #include "ring_buffer/ring_buffer.h"
 
-ring_buffer_t ring;
+ring_buffer_t tx_rb;
+ring_buffer_t rx_rb;
 
-void usart_init();
-void usart_send(const char* string);
-
+void usart_init(volatile usart_t* usart, usart_mode_t* mode, uint32_t baud);
+void usart_send(volatile usart_t* usart, ring_buffer_t* buffer, const char* string);
+void usart_sendbyte(volatile usart_t* usart, ring_buffer_t* buffer, uint8_t byte);
+void usart_processrx(ring_buffer_t* buffer);
 
 int main(){
 
-    ring_buffer_init(&ring);
-    usart_init();
-
+    usart_init(USART0, USART0_MODE, 9600);
+    sei();
+    ring_buffer_init(&tx_rb);
+    ring_buffer_init(&rx_rb);
 
     setOutput(&DDRH, PH6);
 
-    usart_send("hello");
-
-    uint8_t foo = ring_buffer_getcount(&ring);
-
-
     while(1){
-
-    setHigh(&PORTH, PH6);
-
+        usart_processrx(&rx_rb);
     
 
     }
 }
 
-void usart_init(){
-    usart_tx_enable(USART0);
-    usart_rx_enable(USART0);
-    usart_set_baudrate(USART0, USART0_MODE, 9600);
+void usart_init(volatile usart_t* usart, usart_mode_t* mode, uint32_t baud){
+    usart_tx_enable(usart);
+    usart_rx_enable(usart);
+    usart_enable_rx_complete_isr(usart);
+    usart_set_baudrate(usart, mode, baud);
 }
 
-void usart_send(const char* string){
+void usart_processrx(ring_buffer_t* buffer){
+    uint8_t byte;
+    while(ring_buffer_getcount(buffer) > 0){
+        byte = ring_buffer_read(buffer);
+        if (byte == 'l'){
+            setHigh(&PORTH, PH6);
+        } else {
+            setLow(&PORTH, PH6);
+        }
+    }
+}
+
+void usart_send(volatile usart_t* usart, ring_buffer_t* buffer, const char* string){
     while (*string != '\0'){
-        ring_buffer_write(&ring, *string);
+        ring_buffer_write(buffer, *string);
         string++;
     }
-    sei();
-    usart_enable_buffempty_isr(USART0);
+    usart_enable_buffempty_isr(usart);
+}
+
+void usart_sendbyte(volatile usart_t* usart, ring_buffer_t* buffer, uint8_t byte){
+    usart_enable_buffempty_isr(usart);
+    ring_buffer_write(buffer, byte);
 }
 
 
 ISR(USART0_UDRE_vect){
-    if(ring_buffer_empty(&ring)){
+    if(ring_buffer_empty(&tx_rb)){
         usart_disable_buffempty_isr(USART0);
     }
-    USART0->UDRn = ring_buffer_read(&ring);
+    USART0->UDRn = ring_buffer_read(&tx_rb);
+}
+
+ISR(USART0_RX_vect){
+    uint8_t data = USART0->UDRn;
+    ring_buffer_write(&rx_rb, data);
+    if(USART0->UCSRnA & USART_BUFF_EMPTY){
+        USART0->UDRn = data;
+    }
 }
