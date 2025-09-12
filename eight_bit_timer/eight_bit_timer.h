baca833e71c1819c2e31438acd57c8403652f07c
V0874 <visthemasteranon@gmail.com>
2025-09-12 14:33:15 -0500

    continuing to flesh out the 8 bit timer interface

--- eight_bit_timer/eight_bit_timer.h
+++ eight_bit_timer/eight_bit_timer.h
@@ -9,5 +9,31 @@ typedef struct {
     uint8_t OCRnB;                                                                         
 } timer8bit_t;
 
+typedef struct {
+    uint8_t TIMSKn
+} timer8bit_interrupts_t;
+
+typedef struct {
+    uint8_t TIFRn
+} timer8bit_isr_flags_t;
+
+typedef struct {
+    uint8_t ASSRn
+} timer8bit_async_status_t;
+
+typedef struct {
+    uint8_t GTCCRn
+} timer8bit_async_control_t;
+
 #define TIMER0 ((volatile timer8bit_t*) TIMER0_BASE)                       
-#define TIMER2 ((volatile timer8bit_t*) TIMER2_BASE)   
+#define TIMER2 ((volatile timer8bit_t*) TIMER2_BASE)
+
+#define TIMER0_ISR ((volatile timer8bit_interrupts_t*) TIMER0_ISR_BASE)
+#define TIMER2_ISR ((volatile timer8bit_interrupts_t*) TIMER2_ISR_BASE)
+
+#define TIMER0_ISR_FLAGS ((volatile timer8bit_isr_flags_t*) TIMER0_ISR_FLAGS_BASE)
+#define TIMER2_ISR_FLAGS ((volatile timer8bit_isr_flags_t*) TIMER2_ISR_FLAGS_BASE)
+
+#define TIMER2_ASYNC_STATUS ((volatile timer8bit_async_status_t*) TIMER2_ASYNC_STATUS_BASE)
+#define TIMER2_ASYNC_CTRL ((volatile timer8bit_async_control_t*) TIMER2_ASYNC_CTRL_BASE)
+
--- eight_bit_timer/eight_bit_timer_defs.h
+++ eight_bit_timer/eight_bit_timer_defs.h
@@ -3,10 +3,51 @@
                                                         Timer base peripheral block addresses
 */
 
-#define TIMER0_BASE             (0x24UL)
-#define TIMER2_BASE             (0xB0UL)
+#define TIMER0_BASE                 (0x24UL)
+#define TIMER2_BASE                 (0xB0UL)
+
+#define TIMER0_ISR_BASE             (0x6EUL)
+#define TIMER2_ISR_BASE             (0x70UL)
+
+#define TIMER0_ISR_FLAGS_BASE       (0x15UL)
+#define TIMER2_ISR_FLAGS_BASE       (0x17UL)
+
+#define TIMER2_ASYNC_STATUS_BASE    (0xB6UL)
+#define TIMER2_ASYNC_CTRL_BASE      (0x23UL)
 
 /*
 --------------------------------------------------------------------------------------------------------------------------------------------
                                                         Timer flag and configuration settings
 */
+/*
+                                                the 8 bit timer is automatically set in normal mode by default;
+                                                however, by default the timer is configured to a maximum value of 255;
+                                                furthermore, the timer overflow flag gets set once the timer reaches the max value
+                                                , as well as if a value is set in the OCRnX register,it is immediately updated
+                                                and a compare match will be achieved once the timer reaches the specified value.
+*/
+
+
+#define PWM_PHASE_CORRECT_MODE      (1 << 0)    /* sets the 8 bit timer to PWM phase correct mode
+                                                the max value of the timer is configured to be 255, however, the OCRnx register
+                                                value will only be set once the timer reaches the max value, furthermore,
+                                                the timer overflow flag will be set once the timer wraps back to 0
+                                                if the MODE2 setting is set, the max value of the timer can effectively be set to 
+                                                what value the OCRnA register holds */
+
+#define CTC_MODE                    (1 << 1)    /* sets the 8 bit timer to clear timer on compare match mode;
+                                                CTC mode uses the OCRnA register as the top timer value;
+                                                but can create an overflow event if OCRnA is set to max: (255)   */
+
+#define FAST_PWM_MODE               (1 << 0) | (1 << 1) /*sets the 8 bit timer to fast PWM mode; the max value of the timer is
+                                                        configured to be 255, and the value specified in OCRx will be updated
+                                                        once the timer reaches the max value; furthermore, the timer overflow 
+                                                        flag will be set once the timer reaches the max value; if the MODE2 setting
+                                                        is set, the max value of the timer can effectively be set to what value
+                                                        the OCRnA register holds*/
+
+#define PWM_PHASE_CORRECT_MODE2     (1 << 3)    /* this setting changes the mode to allow the top of the timer to be set to the value
+                                                of OCRnA*/
+                                                
+#define FAST_PWM_MODE_MODE2         (1 << 3)    /* this setting changes the mode to allow the top of the timer to be set to the value
+                                                of OCRnA*/
