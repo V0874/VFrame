b3fa6c1163c0c362c5dfdb8bd58992ab15fe4713
V0874 <visthemasteranon@gmail.com>
2025-10-18 14:43:08 -0500

    wrapped up the analog comparator interface

--- ac/ac.h
+++ ac/ac.h
@@ -16,6 +16,27 @@ typedef struct{
 } ac_digi_input_t;
 
 #define AC0 ((volatile ac_t*) AC_BASE)
-#define AC_CTRL ((volatile ac_ctrl_t*) AC_CTRL_STATUS_BASE)
-#define AC_DIGI_INPUT ((volatile ac_digi_input_t*) AC_DIGITAL_INPUT_BASE)
+#define AC0_CTRL ((volatile ac_ctrl_t*) AC_CTRL_STATUS_BASE)
+#define AC0_DIGI_INPUT ((volatile ac_digi_input_t*) AC_DIGITAL_INPUT_BASE)
 
+void ac_set_multiplexer(ac_t* ac, int multiplexer);
+
+void ac_enable(ac_ctrl_t* ac);
+
+void ac_disable(ac_ctrl_t* ac);
+
+void ac_set_bandgap_fixed(ac_ctrl_t* ac);
+
+void ac_set_bandgap_ain0(ac_ctrl_t* ac);
+
+void ac_enable_interrupt(ac_ctrl_t* ac);
+
+void ac_disable_interrupt(ac_ctrl_t* ac);
+
+void ac_input_capture_enable(ac_ctrl_t* ac);
+
+void ac_input_capture_disable(ac_ctrl_t* ac);
+
+void ac_interrupt_mode_select(ac_ctrl_t* ac, uint8_t mode);
+
+void ac_digital_input_disable(ac_ctrl_t* ac, uint8_t ain_pin);
