96a7e3930d45bb8060499425d23260c89f9d3903
V0874 <visthemasteranon@gmail.com>
2025-11-01 23:24:13 -0500

    continued to flesh out our 16 bit timer interface, we are almost done with all of our interfaces but will need loads of documentation

--- 16_bit_timer/16_bit_timer.h
+++ 16_bit_timer/16_bit_timer.h
@@ -16,35 +16,64 @@ typedef struct{
     uint8_t OCRnBH;
     uint8_t OCRnCL;
     uint8_t OCRnCH;
-} timer_16bit_t;
+} timer16bit_t;
 
 typedef struct{
     uint8_t TIMSKn;
-} timer_16bit_interrupts_t;
+} timer16bit_interrupts_t;
 
 typedef struct{
     uint8_t TIFRn;
-} timer_16bit_isr_flags_t;
+} timer16bit_isr_flags_t;
 
 typedef struct{
     uint8_t GTCCRn;
-} timer_16bit_sync_ctrl_t;
+} timer16bit_sync_ctrl_t;
 
 
-#define TIMER1 ((volatile timer_16bit_t*) TIMER1_BASE)
-#define TIMER3 ((volatile timer_16bit_t*) TIMER3_BASE)
-#define TIMER4 ((volatile timer_16bit_t*) TIMER4_BASE)
-#define TIMER5 ((volatile timer_16bit_t*) TIMER5_BASE)
+#define TIMER1 ((volatile timer16bit_t*) TIMER1_BASE)
+#define TIMER3 ((volatile timer16bit_t*) TIMER3_BASE)
+#define TIMER4 ((volatile timer16bit_t*) TIMER4_BASE)
+#define TIMER5 ((volatile timer16bit_t*) TIMER5_BASE)
 
-#define TIMER1_INTERRUPTS ((volatile timer_16bit_interrupts_t*) TIMER1_ISR_BASE)
-#define TIMER3_INTERRUPTS ((volatile timer_16bit_interrupts_t*) TIMER3_ISR_BASE)
-#define TIMER4_INTERRUPTS ((volatile timer_16bit_interrupts_t*) TIMER4_ISR_BASE)
-#define TIMER5_INTERRUPTS ((volatile timer_16bit_interrupts_t*) TIMER5_ISR_BASE)
+#define TIMER1_INTERRUPTS ((volatile timer16bit_interrupts_t*) TIMER1_ISR_BASE)
+#define TIMER3_INTERRUPTS ((volatile timer16bit_interrupts_t*) TIMER3_ISR_BASE)
+#define TIMER4_INTERRUPTS ((volatile timer16bit_interrupts_t*) TIMER4_ISR_BASE)
+#define TIMER5_INTERRUPTS ((volatile timer16bit_interrupts_t*) TIMER5_ISR_BASE)
 
-#define TIMER1_ISR_FLAGS ((volatile timer_16bit_isr_flags_t*) TIMER1_ISR_FLAGS_BASE)
-#define TIMER3_ISR_FLAGS ((volatile timer_16bit_isr_flags_t*) TIMER3_ISR_FLAGS_BASE)
-#define TIMER4_ISR_FLAGS ((volatile timer_16bit_isr_flags_t*) TIMER4_ISR_FLAGS_BASE)
-#define TIMER5_ISR_FLAGS ((volatile timer_16bit_isr_flags_t*) TIMER5_ISR_FLAGS_BASE)
+#define TIMER1_ISR_FLAGS ((volatile timer16bit_isr_flags_t*) TIMER1_ISR_FLAGS_BASE)
+#define TIMER3_ISR_FLAGS ((volatile timer16bit_isr_flags_t*) TIMER3_ISR_FLAGS_BASE)
+#define TIMER4_ISR_FLAGS ((volatile timer16bit_isr_flags_t*) TIMER4_ISR_FLAGS_BASE)
+#define TIMER5_ISR_FLAGS ((volatile timer16bit_isr_flags_t*) TIMER5_ISR_FLAGS_BASE)
 
-#define TIMER_SYNC_CTRL ((volatile timer_16bit_sync_ctrl_t*) TIMER_SYNC_CTRL_BASE)
+#define TIMER_SYNC_CTRL ((volatile timer16bit_sync_ctrl_t*) TIMER_SYNC_CTRL_BASE)
 
+uint16_t timer16bit_read_timer_value(timer16bit_t* timer);
+
+void timer16bit_set_timer_value(timer16bit_t* timer, uint16_t value);
+
+uint16_t timer16bit_read_output_compare_a(timer16bit_t* timer);
+
+void timer16bit_set_output_compare_a_value(timer16bit_t* timer, uint16_t value);
+
+uint16_t timer16bit_read_output_compare_b_value(timer16bit_t* timer);
+
+void timer16bit_set_output_compare_b_value(timer16bit_t* timer, uint16_t value);
+
+uint16_t timer16bit_read_output_compare_c_value(timer16bit_t* timer);
+
+void timer16bit_set_output_compare_c_value(timer16bit_t* timer, uint16_t value);
+
+uint16_t timer16bit_read_input_capture_value(timer16bit_t* timer);
+
+void timer16bit_set_input_capture_value(timer16bit_t* timer, uint16_t value);
+
+void timer16bit_input_capture_enable_interrupt(timer16bit_interrupts_t* timer);
+
+void timer16bit_output_compare_c_match_interrupt_enable(timer16bit_interrupts_t* timer);
+
+void timer16bit_output_compare_b_match_interrupt_enable(timer16bit_interrupts_t* timer);
+
+void timer16bit_output_compare_a_match_interrupt_enable(timer16bit_interrupts_t* timer);
+
+void timer16bit_overflow_interrupt_enable(timer16bit_interrupts_t* timer);
