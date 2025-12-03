2ee475524c432d0c5ad11c68a6b223f23d6b8171
V0874 <visthemasteranon@gmail.com>
2025-12-02 23:12:36 -0600

    changed

--- gpio/gpio.h
+++ gpio/gpio.h
@@ -4,4 +4,4 @@ void setInput(volatile uint8_t* ddr, int pin);
 void setOutput(volatile uint8_t* ddr, int pin);
 void setLow(volatile uint8_t* port, int pin);
 void setHigh(volatile uint8_t* port, int pin);
-bool readPin(volatile uint8_t* port, int pin);
+bool readPin(volatile uint8_t* mainpin, int pin);
